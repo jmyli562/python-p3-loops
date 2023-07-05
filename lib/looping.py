f35e34ff6141b9b7a7b406e02d420a990994b10e
Jimmy Li <jmyli562@gmail.com>
2023-07-05 15:47:17 -0700

    pass test

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 1:
+        if i == 1:
+            print(i)
+            print("Happy New Year!")
+            break
+        else:
+            print(i)
+            i = i - 1
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [num**2 for num in int_list]
+    return int_list
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
